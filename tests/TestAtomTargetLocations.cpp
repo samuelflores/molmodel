f32494b619b6c36e28532d20ec83ff062b0088ca
Michal Malý <madcatxster@devoid-pointer.net>
2021-03-25 14:53:01 +0100

    Fix AtomTargetLocations test

--- tests/TestAtomTargetLocations.cpp
+++ tests/TestAtomTargetLocations.cpp
@@ -43,22 +43,22 @@ try
       // TODO - this should be a very small protein
       // Use slight modifications of default configuration to test the import of state
       String inputPdb = ""
-"ATOM      1  N   HIS    19      28.165  29.227  23.618  1.00 91.78           N\n"
-"ATOM      2  CA  HIS    19      27.004  29.173  22.731  1.00 91.74           C\n"
-"ATOM      3  C   HIS    19      26.321  27.818  22.666  1.00 81.05           C\n"
-"ATOM      4  O   HIS    19      25.105  27.739  22.805  1.00 89.56           O\n"
-"ATOM      5  CB  HIS    19      27.248  29.629  21.270  1.00 98.21           C\n"
-"ATOM      6  CG  HIS    19      27.954  30.936  21.082  1.00100.00           C\n"
-"ATOM      7  ND1 HIS    19      28.852  31.112  20.015  1.00100.00           N\n"
-"ATOM      8  CD2 HIS    19      27.882  32.111  21.798  1.00100.00           C\n"
-"ATOM      9  CE1 HIS    19      29.310  32.368  20.116  1.00100.00           C\n"
-"ATOM     10  NE2 HIS    19      28.753  32.997  21.176  1.00100.00           N\n"
-"ATOM     11  N   SER    20      27.057  26.778  22.303  1.00 58.48           N\n"
-"ATOM     12  CA  SER    20      26.412  25.501  22.190  1.00 45.35           C\n"
-"ATOM     13  C   SER    20      26.089  24.909  23.558  1.00 30.40           C\n"
-"ATOM     14  O   SER    20      26.808  25.067  24.542  1.00 29.60           O\n"
-"ATOM     15  CB  SER    20      27.206  24.513  21.344  1.00 49.69           C\n"
-"ATOM     16  OG  SER    20      26.466  23.310  21.049  1.00 22.13           O\n"
+"ATOM      1  N   HIS A  19      28.165  29.227  23.618  1.00 91.78           N\n"
+"ATOM      2  CA  HIS A  19      27.004  29.173  22.731  1.00 91.74           C\n"
+"ATOM      3  C   HIS A  19      26.321  27.818  22.666  1.00 81.05           C\n"
+"ATOM      4  O   HIS A  19      25.105  27.739  22.805  1.00 89.56           O\n"
+"ATOM      5  CB  HIS A  19      27.248  29.629  21.270  1.00 98.21           C\n"
+"ATOM      6  CG  HIS A  19      27.954  30.936  21.082  1.00100.00           C\n"
+"ATOM      7  ND1 HIS A  19      28.852  31.112  20.015  1.00100.00           N\n"
+"ATOM      8  CD2 HIS A  19      27.882  32.111  21.798  1.00100.00           C\n"
+"ATOM      9  CE1 HIS A  19      29.310  32.368  20.116  1.00100.00           C\n"
+"ATOM     10  NE2 HIS A  19      28.753  32.997  21.176  1.00100.00           N\n"
+"ATOM     11  N   SER A  20      27.057  26.778  22.303  1.00 58.48           N\n"
+"ATOM     12  CA  SER A  20      26.412  25.501  22.190  1.00 45.35           C\n"
+"ATOM     13  C   SER A  20      26.089  24.909  23.558  1.00 30.40           C\n"
+"ATOM     14  O   SER A  20      26.808  25.067  24.542  1.00 29.60           O\n"
+"ATOM     15  CB  SER A  20      27.206  24.513  21.344  1.00 49.69           C\n"
+"ATOM     16  OG  SER A  20      26.466  23.310  21.049  1.00 22.13           O\n"
 "END\n";
 
 
@@ -66,8 +66,7 @@ try
       PdbStructure pdbStructure(inStream, PdbStructure::InputType::PDB);
 
       Protein protein("HS");
-      protein.writeDefaultPdb(cout); // OK
-
+      protein.setPdbChainId("A");
       protein.updResidue(ResidueInfo::Index(1)).setPdbResidueNumber(19);
       protein.updResidue(ResidueInfo::Index(2)).setPdbResidueNumber(20);
 
@@ -88,7 +87,7 @@ try
 
       SimTK_ASSERT_ALWAYS(residual < 0.02, "Structure matching was too inaccurate");
 
-      protein.writeDefaultPdb(cout);
+      protein.writeDefaultPdb(cout, protein.getTopLevelTransform());
 
       cout << "PASSED" << endl;
       return 0;
