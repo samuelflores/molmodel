26e8baf61e53d0b54a38b58e5caf19ff3c0853e1
Michal Malý <malym@ibt.cas.cz>
2021-05-20 11:39:23 +0200

    Read Amber99 parameters file correctly in TestBiotypeSelfCode

--- tests/TestBiotypeSelfCode.cpp
+++ tests/TestBiotypeSelfCode.cpp
@@ -1,19 +1,31 @@
 #include "SimTKmolmodel.h"
+#include <exception>
 #include <iostream>
 #include <fstream>
 
 using namespace SimTK;
 using namespace std;
 
-int main() 
-{
-    Biotype argonBiotype = Biotype::Argon();
+#define ASSERT(x, msg) SimTK_ASSERT_ALWAYS(x, msg)
 
-    DuMMForceFieldSubsystem dumm;
-    ifstream tinkerStream("../../resources/tinker_amber99_clean.prm");
-    dumm.populateFromTinkerParameterFile(tinkerStream);
+int main(int argc, char **argv) {
+    ASSERT(argc == 2, "Invalid number of arguments, expected 2");
 
-    // ofstream codeOut("biotype_test.cpp");
+    ifstream tinkerStream(argv[1]);
+    ASSERT(tinkerStream.good(), "Cannot read TinkerAmber99 parameters file");
 
-    Biotype::generateAllBiotypeCode(cout);
+    try {
+        Biotype argonBiotype = Biotype::Argon();
+        DuMMForceFieldSubsystem dumm;
+        dumm.populateFromTinkerParameterFile(tinkerStream);
+
+        Biotype::generateAllBiotypeCode(cout);
+        return 0;
+    } catch (const exception &ex) {
+        cerr << "EXCEPTION THROWN: " << ex.what() << endl;
+        return 1;
+    } catch (...) {
+        cerr << "UNKNOWN EXCEPTION THROWN" << endl;
+        return 1;
+    }
 }
